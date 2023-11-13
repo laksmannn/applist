2b3a128f526adfb8a176dec3e0d96d9b122d7667
dielaksman98 <dielaksman98@gmail.com>
2023-11-14 00:16:45 +0700

    new api recommended

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,26 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse } from "./libs/api-libs"; 
+import { getAnimeResponse, getNestedAnimeResponse } from "@/libs/api-libs"; 
 
 const Page = async () => {
-  const topAnime = await getAnimeResponse("top/anime", "limit:8")
+  const topAnime = await getAnimeResponse("top/anime", "limit=12")
+  let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
+  recommendedAnime = {data : recommendedAnime.slice(0,8)}
 
   return (
     <>
       {/* anime populer */}
       <section>
         <Header
-          title="favorit"
+          title="Favorite"
           linkTitle="ALL"
           linkHref="/populer"
         />
         <AnimeList api={topAnime} />
+      </section>
+      <section>
+        <Header title="Recommended"/>
+        <AnimeList api={recommendedAnime} />
       </section>
     </>
   );
